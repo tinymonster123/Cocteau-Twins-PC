01394146dae70e3455b90b2121486a08f449308b
tinymonster123 <lfhxgs123@gmail.com>
2025-09-03 08:34:52 +0800

    fix: 更新图标路径以指向正确的资源位置

--- apps/desktop-ui/src/main/index.ts
+++ apps/desktop-ui/src/main/index.ts
@@ -1,7 +1,7 @@
 import { join } from 'node:path'
 import { electronApp, is, optimizer } from '@electron-toolkit/utils'
 import { app, BrowserWindow, ipcMain, shell } from 'electron'
-import icon from '../../resources/icon.png?asset'
+import icon from '../../../../assets/icons/icon.png'
 
 function createWindow(): void {
   // Create the browser window.
